46fd7ecf8ceb50241901f7ad7f922fc2f61e0dc6
VivekAlhat <vivekalhat14@outlook.com>
2019-12-18 19:05:30 +0530

    Added More Secure Way

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ if re.match('^[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-
     # subprocess.call("ifconfig "+ _interface + " down",shell=True)
     # subprocess.call("ifconfig "+ _interface + " hw ether "+ _newMac, shell=True)
     # subprocess.call("ifconfig "+ _interface + " up",shell=True)
+    # The above way is less secure because in end user can execute multiple commands by manipulating variables
+    # To restrict user from executing unnecessary commands, following way is more preferable
+
+    subprocess.call(["ifconfig", _interface, "down"])
+    subprocess.call(["ifconfig", _interface, "hw", "ether", _newMac])
+    subprocess.call(["ifconfig", _interface, "up"])
     print('[+] Changed MAC address of {} from {} to {}'.format(_interface, netifaces.ifaddresses(_interface)[netifaces.AF_LINK][0].get('addr'), _newMac))
 else:
     print('Please enter MAC in the following format (00:11:22:33:44:55)')
