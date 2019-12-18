8614cb9f5c3b8f59c05094c73b7498f8bb6db751
VivekAlhat <vivekalhat14@outlook.com>
2019-12-18 18:36:04 +0530

    Basic Formatting

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import netifaces
 import subprocess
 
 print(netifaces.interfaces())
-_interface = input('Type the interface: ')
+_interface = input('Which interface? > ')
 print('Current MAC address for {} is {}'.format(_interface, netifaces.ifaddresses(_interface)[netifaces.AF_LINK][0].get('addr')))
 print('[+] Changing MAC address for {}'.format(_interface))
-_newMac = input('Enter the new MAC address: ')
+_newMac = input('New MAC address > ')
 
 if re.match('^[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}$', _newMac):
-    subprocess.call("ifconfig "+ _interface + " down",shell=True)
-    subprocess.call("ifconfig "+ _interface + " hw ether "+ _newMac, shell=True)
-    subprocess.call("ifconfig "+ _interface + " up",shell=True)
-    print('[+] Changed MAC address of {} to {}'.format(_interface, _newMac))
+    # subprocess.call("ifconfig "+ _interface + " down",shell=True)
+    # subprocess.call("ifconfig "+ _interface + " hw ether "+ _newMac, shell=True)
+    # subprocess.call("ifconfig "+ _interface + " up",shell=True)
+    print('[+] Changed MAC address of {} from {} to {}'.format(_interface, netifaces.ifaddresses(_interface)[netifaces.AF_LINK][0].get('addr'), _newMac))
 else:
     print('Please enter MAC in the following format (00:11:22:33:44:55)')
