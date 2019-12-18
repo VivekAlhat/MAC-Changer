ce57703be10e4db25ef2e89ea6319ed107dacc53
VivekAlhat <vivekalhat14@outlook.com>
2019-12-18 22:41:57 +0530

    Changed To Command Line Args

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ import optparse
 import netifaces
 import subprocess
 
-parser = optparse.OptionParser()
-
-print(netifaces.interfaces())
-_interface = input('Which interface? > ')
-print('Current MAC address for {} is {}'.format(_interface, netifaces.ifaddresses(_interface)[netifaces.AF_LINK][0].get('addr')))
-print('[+] Changing MAC address for {}'.format(_interface))
-_newMac = input('New MAC address > ')
-
-if re.match('^[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}$', _newMac):
+def macChanger(_interface,_newMac):
     # subprocess.call("ifconfig "+ _interface + " down",shell=True)
     # subprocess.call("ifconfig "+ _interface + " hw ether "+ _newMac, shell=True)
     # subprocess.call("ifconfig "+ _interface + " up",shell=True)
-    
     # The above way is less secure because end user can execute multiple commands by manipulating variables
-    # To restrict user from executing unnecessary commands, following way is more preferable
 
-    subprocess.call(["ifconfig", _interface, "down"])
-    subprocess.call(["ifconfig", _interface, "hw", "ether", _newMac])
-    subprocess.call(["ifconfig", _interface, "up"])
-    print('[+] Changed MAC address of {} from {} to {}'.format(_interface, netifaces.ifaddresses(_interface)[netifaces.AF_LINK][0].get('addr'), _newMac))
-else:
-    print('Please enter MAC in the following format (00:11:22:33:44:55)')
+    # To restrict user from executing unnecessary commands, following way is more preferable
+    if(_interface in netifaces.interfaces()):
+        if re.match('^[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}$', _newMac):
+            subprocess.call(["ifconfig", _interface, "down"])
+            subprocess.call(["ifconfig", _interface, "hw", "ether", _newMac])
+            subprocess.call(["ifconfig", _interface, "up"])
+            print('Current MAC address for {} is {}'.format(_interface, netifaces.ifaddresses(_interface)[netifaces.AF_LINK][0].get('addr')))
+            # print('[+] Changing MAC address for {}'.format(_interface))
+            print('[+] Changed MAC address of {} from {} to {}'.format(_interface, netifaces.ifaddresses(_interface)[netifaces.AF_LINK][0].get('addr'), _newMac))
+        else:
+            print('Please enter MAC in the following format (00:11:22:33:44:55)')
+    else:
+        print('Your machine only have following interfaces {}'.format(netifaces.interfaces()))
+
+# Command Line Arguments
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Your Network Interface (Run ifconfig to see available interfaces)")
+parser.add_option("-m", "--mac", dest="new_MACaddr", help="New MAC address (e.g = 00:11:22:33:44:55)")
+(opt, args) = parser.parse_args()
+
+_interface = opt.interface
+_newMac = opt.new_MACaddr
+macChanger(_interface,_newMac)
